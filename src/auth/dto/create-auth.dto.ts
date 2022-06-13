6697911a718535770d339e53f00a7f7fd8a0373a
Dang Tran Nam <dangtrannam2001@gmail.com>
2022-06-13 22:29:55 +0700

    validation for password

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,7 +1,23 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsNotEmpty,
+  MaxLength,
+  MinLength,
+  IsString,
+  Matches,
+} from 'class-validator';
 export class CreateAuthDto {
   @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
   @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
   password: string;
 }
